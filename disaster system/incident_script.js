11ca6892a73f0178ac8489ba2c08631f9074270b
Solahxiv <115059705+Solahxiv@users.noreply.github.com>
2025-01-09 07:49:20 +0800

    Delete disaster system directory

--- disaster system/incident_script.js
+++
@@ -1,8 +0,0 @@
-// Mobile Navigation Functionality
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-// Toggle Navigation Menu
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
